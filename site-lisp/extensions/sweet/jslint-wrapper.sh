5a14f1a7aefa959c35a1257ffce3667aee2b230b
sweet66 <leibowen66@gmail.com>
2022-11-02 15:53:19 +0800

    remove

--- site-lisp/extensions/sweet/jslint-wrapper.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
- 
-# Add some options to make jslint shutup.
-~/.npm/jslint/0.1.8/package/bin/jslint.js $@ -vars -eqeq -plusplus -browser -sloppy -white | sed 'N;s/\n/\I BLAME BOTH FLYMAKE AND JSLINT/'
-
